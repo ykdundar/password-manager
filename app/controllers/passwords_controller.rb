8eb3a722bc3617ca6e6d8e0000eac720bb082b4a
Yesim Dundar <kuranyesim@gmail.com>
2023-11-28 00:25:43 +0100

    Create new password page

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,3 +1,20 @@
 def index
   @passwords = current_user.passwords
 end
+def new
+  @password = Password.new
+end
+def create
+  @password = current_user.passwords.new(password_params)
+  if @Password.save
+    redirect_to @pasword
+  else
+    render :new, status: :unprocessable_entity
+  end
+end
+
+private
+def password_params
+  params.require(:password).permit(:url, :username, :password)
+end
+end
