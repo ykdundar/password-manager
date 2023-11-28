98191e0e8c4398b7b59edfd9fa6f405dc663874d
Yesim Dundar <kuranyesim@gmail.com>
2023-11-28 01:01:50 +0100

    Debug create action saving user_password and password synchronously

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -2,29 +2,27 @@ class PasswordsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_password, except: [:index, :new, :create]
 
-def index
-  @passwords = current_user.passwords
-end
+  def index
+    @passwords = current_user.passwords
+  end
 
-def new
-  @password = Password.new
-end
   def show
   end
 
-def create
-  @password = current_user.passwords.new(password_params)
-  if @Password.save
-    redirect_to @pasword
-  else
-    render :new, status: :unprocessable_entity
+  def new
+    @password = Password.new
   end
-end
 
-private
-def password_params
-  params.require(:password).permit(:url, :username, :password)
-end
+  def create
+    # Create method saving user_password and password synchronously
+    # If you use save method you have to create password and user password record seperately
+    @password = current_user.passwords.create(password_params)
+    if @password.persisted?
+      redirect_to @password
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
   def password_params
