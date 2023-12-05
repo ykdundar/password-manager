1e2a22c35fa7285bed5e2cc1cc443a6341cc5367
Yesim Dundar <kuranyesim@gmail.com>
2023-12-05 23:42:57 +0100

    Fix indents and debug relationships

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,6 +1,6 @@
 class Password < ApplicationRecord
   has_many :user_passwords, dependent: :destroy
-  has_many :user, through: :user_passwords
+  has_many :users, through: :user_passwords
 
   encrypts :password
   encrypts :username, deterministic: true
