8d8f9cbd74e8856fad7f9130ab338e6cda8520e9
Yesim Dundar <kuranyesim@gmail.com>
2023-12-09 02:29:56 +0100

    Define and validate roles

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -1,4 +1,11 @@
 class UserPassword < ApplicationRecord
+  # Viewer can only view
+  # Editor can update the password
+  # Owner view, edit, and share the password
+  ROLES = %w{owner viewer editor}
+
   belongs_to :user
   belongs_to :password
+
+  validates :role, presence: true, inclusion: {in: ROLES}
 end
