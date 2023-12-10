d705f53fc734e5ff60df7b86062368816ed7d5c9
Yesim Dundar <kuranyesim@gmail.com>
2023-12-10 23:09:12 +0100

    Make viewer automatically selected in the list

--- app/models/user_password.rb
+++ app/models/user_password.rb
@@ -2,13 +2,14 @@ class UserPassword < ApplicationRecord
   # Viewer can only view
   # Editor can update the password
   # Owner view, edit, and share the password
-  ROLES = %w{owner viewer editor}
+  ROLES = %w{viewer editor owner}
 
   belongs_to :user
   belongs_to :password
 
   validates :role, presence: true, inclusion: {in: ROLES}
-
+  
+  attributes :role, default :viewe
   def owner?
     role == "owner"
   end
